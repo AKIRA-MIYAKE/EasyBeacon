f5179e15ec003903a1f374bb8b2af583300521a6
AKIRA-MIAYEK <akira.aktk.miyake@gmail.com>
2015-05-30 15:16:12 +0900

    Initial Commit

---
+++ EasyBeacon/EasyBeacon.h
@@ -0,0 +1,19 @@
+//
+//  EasyBeacon.h
+//  EasyBeacon
+//
+//  Created by MiyakeAkira on 2015/05/30.
+//  Copyright (c) 2015年 Miyake Akira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyBeacon.
+FOUNDATION_EXPORT double EasyBeaconVersionNumber;
+
+//! Project version string for EasyBeacon.
+FOUNDATION_EXPORT const unsigned char EasyBeaconVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyBeacon/PublicHeader.h>
+
+
